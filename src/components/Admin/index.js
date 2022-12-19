8808545fefecbbbfc486cefc1e0833ae47c65386
github.arief <78076941+arief-github@users.noreply.github.com>
2022-12-19 21:13:52 +0700

    add : basic folder structure and routes

---
+++ src/components/Admin/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminPage = () => {
+	return (
+		<div>
+			<h1> AdminPage </h1>
+		</div>
+	)
+}
+
+export default AdminPage;
---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+const LANDING = '/';
+const SIGN_UP = '/signup';
+const SIGN_IN = '/signin';
+const HOME = '/home';
+const ACCOUNT = '/account';
+const ADMIN = '/admin';
+const PASSWORD_FORGET = '/pw-forget';
+
+export { LANDING, SIGN_UP, SIGN_IN, HOME, ACCOUNT, ADMIN, PASSWORD_FORGET };
