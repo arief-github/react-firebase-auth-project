fe8c7cec6576907f6c6010e4aeac2963a266a4d6
github.arief <78076941+arief-github@users.noreply.github.com>
2022-12-24 09:55:16 +0700

    add : session handling for auth and non-auth user

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navigation from '../Navigation';
@@ -11,21 +11,39 @@ import AccountPage from '../Account';
 import AdminPage from '../Admin';
 
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
 
-const App = () => (
-  <Router>
-    <div>
-      <Navigation/>
-      <hr/>
-      <Route exact path={ROUTES.LANDING} component={LandingPage} />
-      <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
-      <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
-      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget}/>
-      <Route path={ROUTES.HOME} component={HomePage} />
-      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
-      <Route path={ROUTES.ADMIN} component={AdminPage} />
-    </div>
-  </Router>
-)
+const App = ({ firebase }) => {
+  const [authUser, setAuthUser] = useState(null);
 
-export default App;
+  // like componentDidMount()
+  useEffect(() => {
+    const listener = firebase.auth.onAuthStateChanged(authUser => {
+      authUser ? setAuthUser(authUser) : setAuthUser(null)
+    })
+
+    // like componentWillUnmount()
+    return () => {
+      listener();
+    }
+  }, [firebase.auth])
+
+  return (
+    <Router>
+        <div>
+          <Navigation authUser={authUser}/>
+          <hr/>
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage}/>
+          <Route path={ROUTES.SIGN_IN} component={SignInPage}/>
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForget}/>
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+        </div>
+    </Router>
+
+  )
+}
+
+export default withFirebase(App);
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 
-const Navigation = () => (
+const Navigation = ({ authUser }) => (
+	<> {authUser ? <NavigationAuth/> : <NavigationNonAuth/>} </>
+)
+
+const NavigationAuth = () => (
 	<div>
 		<ul>
-			<li>
-				<Link to={ROUTES.SIGN_IN}>Sign In</Link>
-			</li>
-			<li>
-				<Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-			</li>
 			<li>
 				<Link to={ROUTES.LANDING}>Landing</Link>
 			</li>
@@ -22,10 +21,21 @@ const Navigation = () => (
 				<Link to={ROUTES.ACCOUNT}>Account</Link>
 			</li>
 			<li>
-				<Link to={ROUTES.ADMIN}>Admin</Link>
+				<SignOutButton/>
 			</li>
 		</ul>
 	</div>
 )
+
+const NavigationNonAuth = () => (
+	<ul>
+		<li>
+			<Link to={ROUTES.LANDING}>Landing</Link>
+		</li>
+		<li>
+			<Link to={ROUTES.SIGN_IN}>Sign In</Link>
+		</li>
+	</ul>
+);
 
 export default Navigation;
